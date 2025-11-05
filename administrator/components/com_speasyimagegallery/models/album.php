671232fb8bffa3b398a2455f2f8a7502bc7ce516
Md Siddiqur Rahman <53170094+siddik-web@users.noreply.github.com>
2025-11-05 12:14:12 +0600

    chore: update copyright year to 2025 and version to 2.0.5

--- administrator/components/com_speasyimagegallery/models/album.php
+++ administrator/components/com_speasyimagegallery/models/album.php
@@ -2,15 +2,12 @@
 /**
 * @package com_speasyimagegallery
 * @author JoomShaper http://www.joomshaper.com
-* @copyright Copyright (c) 2010 - 2024 JoomShaper
+* @copyright Copyright (c) 2010 - 2025 JoomShaper
 * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or later
 */
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
-
-jimport('joomla.filesystem.folder');
-jimport('joomla.filesystem.file');
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Table\Table;
