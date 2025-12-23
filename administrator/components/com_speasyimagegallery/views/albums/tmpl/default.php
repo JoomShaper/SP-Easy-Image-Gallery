f47e50625dae20b8520ad29fdeec34d7f56f60f1
Susmita-Priya <susmita.saha.cs@gmail.com>
2025-12-23 15:25:52 +0600

    update

--- administrator/components/com_speasyimagegallery/views/albums/tmpl/default.php
+++ administrator/components/com_speasyimagegallery/views/albums/tmpl/default.php
@@ -259,6 +259,5 @@ if ($saveOrder && !empty($this->items)) {
 			<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
 			<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
 			<?php echo HTMLHelper::_('form.token'); ?>
-		</div>
 
 </form>
